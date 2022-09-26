b0feec5af78e9a0d7b61ba645a6a509ff99b470a
Tom Everson <studycafe.125@gmail.com>
2022-09-26 22:44:23 +0630

    Fixes

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./database.db'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False})
+#SQLALCHEMY_DATABASE_URL = 'sqlite:///./database.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:tdclacQGPEEhjuHtKFwz@containers-us-west-67.railway.app:6666/railway'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine,autocommit=False, autoflush=False)
 
